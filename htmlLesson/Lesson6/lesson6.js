9d386671e538598b3b14911093ed7082dd426df8
SimonComeau <sim_com1@hotmail.com>
2015-10-31 13:40:54 -0400

    button toggle

--- htmlLesson/Lesson6/lesson6.js
+++ htmlLesson/Lesson6/lesson6.js
@@ -8,14 +8,28 @@ function displayFooter() {
 
 function contentLoaded() {
     'use strict';
-    setTimeout(displayFooter, 5000);
+    // setTimeout(displayRedDiv, 2000);
+    setTimeout(displayFooter, 2000);
 }
 
-document.addEventListener("DOMContentLoaded", clickHandler);
+// document.addEventListener("DOMContentLoaded", contentLoaded);
 
-function clickHandler() {
+function changeRedDiv() {
     'use strict';
-    document.getElementById('redDiv').children[0].children[1].text = 'Simon';
+    var property = document.getElementById('redDiv');
+    if (property.style.backgroundColor === 'red') {
+        property.style.backgroundColor = 'blue';
+    } else {
+        property.style.backgroundColor = 'red';
+    }
+    var textValue = document.getElementById('redDiv');
+    if (textValue.children[0].innerHTML === 'Brian') {
+        textValue.children[0].innerHTML = 'Simon';
+    } else {
+        textValue.children[0].innerHTML = 'Brian';
+    }
+    // document.getElementById('redDiv').style.background = 'blue';
+    // document.getElementById('redDiv').children[0].innerHTML = 'Simon';
 }
 // document.addEventListener("DOMContentLoaded", function () {
 //     "use strict";
